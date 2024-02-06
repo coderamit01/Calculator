8333aa4767fe821d19c95d4435c25167e5428394
amit <amitmohammad46@gmail.com>
2024-02-06 22:35:54 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#F5CE9F',
+        'secondary': '#FFFFDD',
+      }
+    },
+  },
+  plugins: [],
+  
+}
